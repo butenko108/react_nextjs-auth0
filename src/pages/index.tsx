115f15d53f9491a37a7ece095b1fea097d6b20ca
sandipani <6083341@gmail.com>
2023-09-01 12:16:35 +0300

    chore: reinstall nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <h1>Hello, Next.js Auth0!</h1>
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+export default function Index() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
 }
