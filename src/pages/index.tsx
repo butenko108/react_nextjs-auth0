c43da08fc38689239549dc0fb6923ab77af62f0a
sandipani <6083341@gmail.com>
2023-09-04 10:05:58 +0300

    feat: auth0 implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
 
 export default function Index() {
   const { user, error, isLoading } = useUser();
@@ -9,7 +10,10 @@ export default function Index() {
   if (user) {
     return (
       <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+        {user.picture && <Image src={user.picture} alt='avatar' width={50} height={50} />}
+        <p>Welcome {user.name}!</p>
+        <p>Your email: {user.email}</p>
+        <a href="/api/auth/logout">Logout</a>
       </div>
     );
   }
